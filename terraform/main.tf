68634f28ab5f1d7d044331ffb2ff082b8ffc74cb
lucasmelogithub <lucas.melo@intel.com>
2022-12-07 15:39:07 -0600

    Workflow test

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 module "optimized-mysql-server" {
   source              = "intel/azure-mysql_flexible_server/intel"
   resource_group_name = "terraform-example-app-rg"
-  db_server_name      = "dev-intel-example-app02"
+  db_server_name      = "dev-intel-example-app01"
   db_password         = var.db_password
   tags = {
     Owner    = "Intel.Terraform.Example.App@intel.com"
